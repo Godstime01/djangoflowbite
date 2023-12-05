d20fe8fb5abb7103e744772dba1766cd7d48c9f8
godstime01 <youngestkid01@gmail.com>
2023-12-05 17:19:13 +0100

    g

--- djangoflowbite/settings.py
+++ djangoflowbite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*.fly.dev']  # <-- Updated!
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*.fly.dev', '*']  # <-- Updated!
 
 CSRF_TRUSTED_ORIGINS = ['https://*.fly.dev']
 
